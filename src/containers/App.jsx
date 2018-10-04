3abfa79a005c87282e31683027c9f22ef9bb012b
BlindBadCat <frost1759@gmail.com>
2018-10-04 10:43:12 +0300

    Prop types added, function name fixed

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -7,9 +7,9 @@ import MoviesList from './ShowTable';
 import Buttons from './Buttons';
 
 
-const App = ({ fetchShowsAction, urlParams }) => {
+const App = ({ fetchShowsIfNeededAction, urlParams }) => {
   const getUpdate = () => {
-    fetchShowsAction(urlParams);
+    fetchShowsIfNeededAction(urlParams);
   };
 
   getUpdate();
@@ -24,26 +24,29 @@ const App = ({ fetchShowsAction, urlParams }) => {
 
   );
 };
-/*
-App.PropTypes = {
-  fetchShowsAction: PropTypes.func,
 
-  pagination: PropTypes.shape({
-    pageCount: PropTypes.number,
-    limit: PropTypes.number,
-    currentPage: PropTypes.number,
-    itemCount: PropTypes.number,
-  }),
+App.propTypes = {
+  fetchShowsIfNeededAction: PropTypes.func.isRequired,
+  urlParams: PropTypes.shape({
+    pagination: PropTypes.shape({
+      pageCount: PropTypes.number,
+      limit: PropTypes.number,
+      currentPage: PropTypes.number,
+      itemCount: PropTypes.number,
+    }),
+    searchUrl: PropTypes.string,
+    query: PropTypes.string,
+    genre: PropTypes.string,
 
-  searchUrl: PropTypes.string,
-};*/
+  }).isRequired,
+};
 
 const mapStateToProps = store => ({
   urlParams: store.shows.urlParams,
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchShowsAction: (sort, page, limit) => dispatch(fetchShowsIfNeeded(sort, page, limit)),
+  fetchShowsIfNeededAction: urlParams => dispatch(fetchShowsIfNeeded(urlParams)),
 });
 
 export default connect(
