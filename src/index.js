7874d223504d9f023b7bbc43ea01c16287cf4c40
BlindBadCat <frost1759@gmail.com>
2018-10-04 17:14:29 +0300

    registerServiceWorker added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-/* eslint-disable no-undef,import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/configureStore';
+import registerServiceWorker from './registerServiceWorker';
 import App from './containers/App';
 import './index.css';
-
 
 ReactDOM.render(
   <Provider store={store}>
@@ -13,4 +12,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-// registerServiceWorker();
+registerServiceWorker();
